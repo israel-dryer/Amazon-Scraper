6207bc20b8267ccf51557d83bf308d0b48fb8f89
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2020-12-20 20:36:05 -0500

    bug fixes, improved saving
    the data now saves while scraping instead of waiting until the end. Fixed bugs where price was not getting collected.

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -1,29 +1,37 @@
 """
     Amazon Web scraper
 
-    Modified:   2020-12-07
+    Modified:   2020-12-20
     Author:     Israel Dryer
 """
 import csv
-from datetime import datetime
 from time import sleep
+from datetime import datetime
 from random import random
-from typing import List, Tuple
-from selenium.webdriver.remote import webelement, webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common import exceptions
 from msedge.selenium_tools import Edge, EdgeOptions
 
 
-def save_data_to_csv(data: List[Tuple], filename: str, header: List[str]):
-    """Save data to file"""
-    with open(filename, 'w', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerow(header)
-        writer.writerows(data)
+def generate_filename(search_term):
+    timestamp = datetime.now().strftime("%Y%m%d%H%S%M")
+    stem = path = '_'.join(search_term.split(' '))
+    filename = stem + '_' + timestamp + '.csv'
+    return filename
 
 
-def create_webdriver() -> webdriver:
-    """Create and return an Edge webdriver"""
+def save_data_to_csv(record, filename, new_file=False):
+    header = ['description', 'price', 'rating', 'review_count', 'url']
+    if new_file:
+        with open(filename, 'w', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+    else:
+        with open(filename, 'a+', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerow(record)
+
+
+def create_webdriver():
     options = EdgeOptions()
     options.use_chromium = True
     options.headless = True
@@ -31,63 +39,78 @@ def create_webdriver() -> webdriver:
     return driver
 
 
-def generate_url_template(search_term: str) -> str:
-    """Generate the url based on the search term"""
+def generate_url(search_term, page):
     base_template = 'https://www.amazon.com/s?k={}&ref=nb_sb_noss_1'
     search_term = search_term.replace(' ', '+')
     stem = base_template.format(search_term)
     url_template = stem + '&page={}'
-    return url_template
+    if page == 1:
+        return stem
+    else:
+        return url_template.format(page)
 
 
-def extract_card_data(item: webelement) -> Tuple or None:
-    """Extract data from a single record"""
-    description = item.find_element_by_xpath('.//h2/a').text.strip()
-    url = item.find_element_by_xpath('.//h2/a').get_attribute('href')
+def extract_card_data(card):
+    description = card.find_element_by_xpath('.//h2/a').text.strip()
+    url = card.find_element_by_xpath('.//h2/a').get_attribute('href')
     try:
-        price = item.find_element_by_xpath('.//span[@class="a-price"]//span[@class="a-offscreen"]').text
-    except NoSuchElementException:
+        price = card.find_element_by_xpath('.//span[@class="a-price-whole"]').text
+    except exceptions.NoSuchElementException:
         return
     try:
-        rating = item.find_element_by_tag_name('i').text
-    except NoSuchElementException:
+        temp = card.find_element_by_xpath('.//span[contains(@aria-label, "out of")]')
+        rating = temp.get_attribute('aria-label')
+    except exceptions.NoSuchElementException:
         rating = ""
     try:
-        review_count = item.find_element_by_xpath('.//span[@class="a-size-base" and @dir="auto"]').text
-    except NoSuchElementException:
+        temp = card.find_element_by_xpath('.//span[contains(@aria-label, "out of")]/following-sibling::span')
+        review_count = temp.get_attribute('aria-label')
+    except exceptions.NoSuchElementException:
         review_count = ""
     return description, price, rating, review_count, url
 
 
-def run(search_term: str) -> None:
+def collect_product_cards_from_page(driver):
+    cards = driver.find_elements_by_xpath('//div[@data-component-type="s-search-result"]')
+    return cards
+
+
+def sleep_for_random_interval():
+    time_in_seconds = random() * 3
+    sleep(time_in_seconds)
+
+
+def run(search_term):
     """Run the Amazon webscraper"""
-    records = []
+    filename = generate_filename(search_term)
+    save_data_to_csv(None, filename, new_file=True)  # initialize a new file
     driver = create_webdriver()
-    url_template = generate_url_template(search_term)
+    num_records_scraped = 0
 
-    for page in range(1, 21):  # Max of 20 pages of results can be scraped
-        url = url_template.format(page)
-        print(f"Extracting from: {url}")
-        driver.get(url)
-        sleep(0.5)
-        cards = driver.find_elements_by_xpath('//div[@data-component-type="s-search-result"]')
+    for page in range(1, 21):  # max of 20 pages
+        # load the next page
+        search_url = generate_url(search_term, page)
+        print(search_url)
+        try:  # load the next page
+            driver.get(search_url)
+        except exceptions.TimeoutException:
+            print('TIMEOUT while waiting for page to load')
+            break
+
+        # extract product data
+        cards = collect_product_cards_from_page(driver)
         for card in cards:
-            result = extract_card_data(card)
-            if result:
-                records.append(result)
-        sleep(random() * 3)  # random delay to prevent blocking
+            record = extract_card_data(card)
+            if record:
+                save_data_to_csv(record, filename)
+                num_records_scraped += 1
+        sleep_for_random_interval()
 
-    # shut down the web driver
+    # shut down and report results
     driver.quit()
-
-    # save the data
-    header = ['description', 'price', 'rating', 'review_count', 'url']
-    filename = search_term.replace(' ', '_') + '_' + datetime.today().strftime('%Y%m%d') + '.csv'
-    save_data_to_csv(records, filename, header)
-
-    # notification
-    print(f"Scraped {len(records):,d} for the search term: {search_term}")
+    print(f"Scraped {num_records_scraped:,d} for the search term: {search_term}")
 
 
 if __name__ == '__main__':
-    run('dell laptop')
+    term = 'dell laptop'
+    run(term)
